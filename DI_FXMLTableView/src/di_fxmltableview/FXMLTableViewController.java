4384ad5c9f0cc8b0a7a5e030061f3d9210cd5bca
mgargon25 <marcos.garciagonzalez25@gmail.com>
2021-11-15 20:31:01 +0100

    Ejercicio 2 FXML TableView

---
+++ DI_FXMLTableView/src/di_fxmltableview/FXMLTableViewController.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package di_fxmltableview;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author Usuario
+ */
+public class FXMLTableViewController implements Initializable {
+    
+    @FXML private TableView<Person> tableView;
+    @FXML private TextField firstNameField;
+    @FXML private TextField lastNameField;
+    @FXML private TextField emailField;
+    
+    @FXML
+    protected void addPerson(ActionEvent event) {
+        ObservableList<Person> data = tableView.getItems();
+        data.add(new Person(firstNameField.getText(),
+            lastNameField.getText(),
+            emailField.getText()
+        ));
+        
+        firstNameField.setText("");
+        lastNameField.setText("");
+        emailField.setText("");   
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
